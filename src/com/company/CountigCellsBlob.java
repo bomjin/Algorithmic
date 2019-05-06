053a8ec06ba715133fdf0cf5e506d93fe04e02d9
bumjin <bom96627@naver.com>
2019-05-06 13:04:55 +0900

    5/6 Countig Cells Blob commit

--- src/com/company/CountigCellsBlob.java
+++ src/com/company/CountigCellsBlob.java
@@ -2,33 +2,41 @@ package com.company;
 
 public class CountigCellsBlob {
 
+
+    private static int[][] grid = {
+            {1, 0, 0, 0, 0, 0, 0, 1}, //8x8 셀
+            {1, 0, 1, 0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 0, 0, 0, 1},
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 0, 0},
+            {0, 0, 1, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+    };
     private static int BACKGROUND_COLOR = 0;
     private static int IMAGE_COLOR = 1;
     private static int ALREADY_COUNTED = 2;
     private static int N = 8;
-    private static int[][] grid = {
-            {0, 0, 0, 0, 0, 0, 0, 1}, //8x8 셀
-            {0, 1, 1, 0, 1, 1, 0, 1},
-            {0, 0, 0, 1, 0, 0, 0, 1},
-            {0, 1, 0, 0, 1, 1, 0, 0},
-            {0, 1, 1, 1, 0, 0, 1, 1},
-            {0, 1, 0, 0, 0, 1, 0, 1},
-            {0, 0, 0, 1, 0, 0, 0, 1},
-            {0, 1, 1, 1, 0, 1, 0, 0},
-    };
 
 
 
 public int countCells(int x, int y) {
-    if (x<0 || x>=N || y<0 || y>=N)   // 0~  N-1
+    if (x < 0 || x >= N || y < 0 || y >= N)   // 0~  N-1
         return 0;
     else if (grid[x][y] != IMAGE_COLOR)
         return 0;
-    else {
+    else
         grid[x][y] = ALREADY_COUNTED;
-        return  1 + countCells(x-1,y+1) + countCells(x,y+1) +countCells(x+1,y+1) +countCells(x-1,y+1) +
-                countCells(x-1,y) +countCells(x+1,y) +countCells(x-1,y-1) +countCells(x,y-1) +
-                countCells(x+1,y-1);
-    }
+    return 1 + countCells(x - 1, y + 1) + countCells(x, y + 1) + countCells(x + 1, y + 1) + countCells(x - 1, y + 1) +
+            countCells(x - 1, y) + countCells(x + 1, y) + countCells(x - 1, y - 1) + countCells(x, y - 1) +
+            countCells(x + 1, y - 1);
+
 }
+public static void main(String[] args) {
+    CountigCellsBlob blob = new CountigCellsBlob();
+    int result = blob.countCells(1,2);
+    System.out.println(result);
+
+}
+
 }
